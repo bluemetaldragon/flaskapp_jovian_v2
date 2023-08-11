e529a0dc21d0cb47341baa6d83c2aab0db0e799d
BlueMetalDragon <64070270+bluemetaldragon@users.noreply.github.com>
2023-08-11 12:43:34 +0000

    These files for Jovian's webapp related changes

--- app.py
+++ app.py
@@ -1,7 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+
 
 app = Flask(__name__)
 
+jobs = [
+  {'id':1, 'title':"Sr Dev", 'loc':"Singapore", 'sal':'120k SGD'},
+  {'id':2, 'title':"Jr Dev", 'loc':"Singapore"},
+  {'id':3, 'title':"Sr Mgr", 'loc':"Singapore", 'sal':'150k SGD'},
+]
+
+
+
+
 @app.route("/")
-def hello_world():
-    return render_template("index.html", title="Hello")
+def hellow():
+    return render_template("index.html", jobs=jobs)
+
+
+@app.route("/api/jobs/")
+def return_jobs():
+    return jsonify(jobs)
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
