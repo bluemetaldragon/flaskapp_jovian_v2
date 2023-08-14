579cffa7b37bc6fd618225e8b7a730d02ce76e18
BlueMetalDragon <64070270+bluemetaldragon@users.noreply.github.com>
2023-08-14 02:20:31 +0000

    writes the applicatoin to a DB table

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import os
 from flask import Flask, render_template, jsonify, request
-#from sqlalchemy import create_engine, text
-from database import get_from_db, get_from_db_using_id
+from database import get_from_db, get_from_db_using_id,post_application_to_db
 
 
 
@@ -12,6 +11,8 @@ def hellow():
     """renders the main page"""
     jobs = get_from_db()
     return render_template("index.html", jobs=jobs)
+
+
 
 @app.route("/api/jobs/<id>")
 def return_jobs_from_id(id):
@@ -27,16 +28,21 @@ def return_jobs():
     jobs = get_from_db()
     return jsonify(jobs)
 
+
+
 @app.route("/api/jobs/<id>/apply", methods =['post'])
 def apply_to_job(id):
 #    data = request.args
-    data = request.form
-    return jsonify(data)
 
+    #capture the posted data from the submit button into data variable
+    data = request.form
+    job = get_from_db_using_id(id)
+#    return jsonify(data)
+    status_msg = post_application_to_db(id, data)
+    return render_template('submit_form_to_db.html', app_data=data, job=job, status=status_msg)
 
 
 if __name__ == '__main__':
-
     #to avoid the error of some other process using port 5000
     port = int(os.environ.get("PORT", 3000))
     app.run(host="0.0.0.0", port=port, debug=True)
--- database.py
+++ database.py
@@ -44,7 +44,8 @@ def get_from_db_using_id(id_param):
     )
 
     with engine.connect() as conn:
-        result = conn.execute(text("select * from jobs where id= :val"), {"val": id_param})
+        select_text = text("select * from jobs where id= :val")
+        result = conn.execute(select_text, {"val": id_param})
         # for more details on "result" check https://docs.sqlalchemy.org/en/20/core/connections.html
     
     #tocheck if result was null you need to use rowcount, fetch etc
@@ -54,5 +55,33 @@ def get_from_db_using_id(id_param):
             result_jobs_dict.append (dict(x._mapping))
 
         return result_jobs_dict
+    else:
+        return None
+
+
+# new function to post application data to DB
+def post_application_to_db(id, data):
+    #get handle on the DB to write
+    connect_string = "mysql+pymysql://516ae3996bu1xn6nyc6f:pscale_pw_yBdiPHEMHW91AOU4ODP0x1aPgvZqUZ0y6ve0dxi8K0h@aws.connect.psdb.cloud/flaskapp_jovian_v2"
+    engine = create_engine(
+        connect_string,
+        connect_args={
+            "ssl": {
+                "ssl_ca": "/etc/ssl/cert.pem",
+            }
+        }
+    )
+
+    #write to DB "data" based on "id"
+    with engine.connect() as conn:
+        select_text = text("insert into job_apps (job_id,full_name, email, url) values(:job_id, :full_name, :email, :url)")
+        result = conn.execute(select_text, {"job_id": id, "full_name": data['full_name'], "email": data['email'], "url": data['url']})
+        # for more details on "result" check https://docs.sqlalchemy.org/en/20/core/connections.html
+    
+    #tocheck if result was null you need to use rowcount, fetch etc
+    if result.rowcount !=0:
+        success_msg= "Congratulations, your application is posted"
+        return success_msg
+
     else:
         return None
